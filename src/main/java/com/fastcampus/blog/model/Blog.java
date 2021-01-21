7f815875fb75f89ec21bf9038d9d14e432c76670
FoeverNa <foevna@naver.com>
2021-01-22 08:31:42 +0900

    Add columns on Blog Entity

--- src/main/java/com/fastcampus/blog/model/Blog.java
+++ src/main/java/com/fastcampus/blog/model/Blog.java
@@ -1,4 +1,25 @@
 package com.fastcampus.blog.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Blog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @OneToOne
+    private Member member;
+
+    @OneToMany
+    private List<Post> postList;
+
+
+
+
 }
