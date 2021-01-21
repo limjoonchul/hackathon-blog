0a448038ec7c2fa955f47b4b77b62bc04d5bee5b
FoeverNa <foevna@naver.com>
2021-01-22 08:44:07 +0900

    Add Columns on Post

--- src/main/java/com/fastcampus/blog/model/Post.java
+++ src/main/java/com/fastcampus/blog/model/Post.java
@@ -1,4 +1,34 @@
 package com.fastcampus.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    private Blog blog;
+
+    @ManyToOne
+    private Member member;
+
+    @OneToMany
+    private List<Comment> comments;
+
 }
