e95bf23bc1ba6d820e9ccd32ccd3965a4deb243d
FoeverNa <foevna@naver.com>
2021-01-22 08:46:55 +0900

    Add Columns on Comment

--- src/main/java/com/fastcampus/blog/model/Comment.java
+++ src/main/java/com/fastcampus/blog/model/Comment.java
@@ -1,4 +1,29 @@
 package com.fastcampus.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    private Post post;
+
+    @ManyToOne
+    private Member member;
 }
