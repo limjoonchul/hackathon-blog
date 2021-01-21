4b9dfb6ac86a608d66575dea5df3ee208eb2e914
FoeverNa <foevna@naver.com>
2021-01-22 08:57:50 +0900

    Add Columns on Member

--- src/main/java/com/fastcampus/blog/model/Member.java
+++ src/main/java/com/fastcampus/blog/model/Member.java
@@ -1,4 +1,33 @@
 package com.fastcampus.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "deleted = false")
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @OneToOne
+    private Blog blog;
+
+    @ColumnDefault("false")
+    private boolean deleted;
+
 }
