18c3aea729b38769742d4d02891ab8495156093a
limjoonchul <dlawnscjf511@naver.com>
2021-01-22 13:27:31 +0900

    feat: add getCommentById() on CommentService

---
+++ src/main/java/com/fastcampus/blog/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.fastcampus.blog.service;
+
+import com.fastcampus.blog.dto.CommentDto;
+import com.fastcampus.blog.model.Comment;
+import com.fastcampus.blog.model.Post;
+import com.fastcampus.blog.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public Comment getCommentById(Long id){
+
+        Optional<Comment> findComment = commentRepository.findById(id);
+
+        return findComment.orElseGet(() -> findComment.orElse(new Comment()));
+    }
+}
