4414104bde9c3eae085bf29f9438323614038e85
FoeverNa <foevna@naver.com>
2021-01-22 16:54:04 +0900

    test : Add getPostsTest

--- src/test/java/com/fastcampus/blog/controller/PostControllerTest.java
+++ src/test/java/com/fastcampus/blog/controller/PostControllerTest.java
@@ -1,14 +1,19 @@
 package com.fastcampus.blog.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class PostControllerTest {
@@ -21,5 +26,13 @@ class PostControllerTest {
                 .addFilter(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(print())
                 .build();
+    }
+
+    @Test
+    void getPostsTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].title").value("title"))
+                .andExpect(jsonPath("$.[0].content").value("content"));
     }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -2,3 +2,5 @@ Insert Into Blog(id, name) values (1L, 'MyBlog')
 
 Insert Into Member(id, email, nickname, password) values (1L, 'martin@fastcampus.com', 'martin', 'lovemartin')
 Insert Into Comment(id, content, member_id) values (1L, '제목입니다', 1L)
+
+Insert Into Post(id, title, content) values (1L, 'title', 'content')
