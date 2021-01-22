7cd3c4fa55575aa59c0368937d0a37e3be848ee2
FoeverNa <foevna@naver.com>
2021-01-22 10:48:41 +0900

    feat: Add findPostByID

--- src/main/java/com/fastcampus/blog/repository/PostRepository.java
+++ src/main/java/com/fastcampus/blog/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.fastcampus.blog.repository;
 import com.fastcampus.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findPostById(Long id);
 }
