17a404b28f60e02c0d2f481bc3071616b32bc511
limjoonchul <dlawnscjf511@naver.com>
2021-01-22 17:09:52 +0900

    fix: deleted @NonNull

---
+++ src/main/java/com/fastcampus/blog/dto/CommentDto.java
@@ -0,0 +1,28 @@
+package com.fastcampus.blog.dto;
+
+import com.fastcampus.blog.model.Member;
+import com.fastcampus.blog.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+
+
+    private Long id;
+
+    @NotEmpty
+    private String content;
+
+    private Post post;
+
+    private Member member;
+
+
+}
