89cd19062531153fe697f54da49fdc62849ee5b8
FoeverNa <foevna@naver.com>
2021-01-22 12:08:17 +0900

    feat: Create PostService

---
+++ src/main/java/com/fastcampus/blog/service/PostService.java
@@ -0,0 +1,14 @@
+package com.fastcampus.blog.service;
+
+import com.fastcampus.blog.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+
+}
