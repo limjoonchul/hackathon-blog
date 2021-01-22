b4f5b622804576b44155dbb5e31a0b430bff0776
FoeverNa <foevna@naver.com>
2021-01-22 16:29:35 +0900

    test : Create PostControllerTest & init

---
+++ src/test/java/com/fastcampus/blog/controller/PostControllerTest.java
@@ -0,0 +1,25 @@
+package com.fastcampus.blog.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+class PostControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void before(WebApplicationContext wac) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+}
