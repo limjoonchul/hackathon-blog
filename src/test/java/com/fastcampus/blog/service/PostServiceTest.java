398f7c99e249c451671c2bccb7223dd42c95ce60
FoeverNa <foevna@naver.com>
2021-01-22 13:44:16 +0900

    test: Add getPostsTest

--- src/test/java/com/fastcampus/blog/service/PostServiceTest.java
+++ src/test/java/com/fastcampus/blog/service/PostServiceTest.java
@@ -1,12 +1,18 @@
 package com.fastcampus.blog.service;
 
+import com.fastcampus.blog.model.Post;
 import com.fastcampus.blog.repository.PostRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -16,6 +22,28 @@ class PostServiceTest {
     @Mock
     private PostRepository postRepository;
 
+    @Test
+    void getPostsTest(){
+        when(postRepository.findAll()).thenReturn(mockPosts());
+
+        List<Post> result = postService.getPosts();
+
+        System.out.println(">>>" + result);
+
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getTitle()).isEqualTo("mockTestTitle");
+
+    }
+
+    private List<Post> mockPosts() {
+        List<Post> posts = new ArrayList<>();
+        Post post = new Post();
+        post.setTitle("mockTestTitle");
+
+        posts.add(post);
+
+        return posts;
+    }
 
 
 }
