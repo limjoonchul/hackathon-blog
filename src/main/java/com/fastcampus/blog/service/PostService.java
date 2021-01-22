bcff5fb213bc0c8b8a097c5aba182b0d61e37442
FoeverNa <foevna@naver.com>
2021-01-22 13:06:32 +0900

    feat: Add updatePost

--- src/main/java/com/fastcampus/blog/service/PostService.java
+++ src/main/java/com/fastcampus/blog/service/PostService.java
@@ -1,5 +1,6 @@
 package com.fastcampus.blog.service;
 
+import com.fastcampus.blog.dto.PostDto;
 import com.fastcampus.blog.model.Post;
 import com.fastcampus.blog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +22,29 @@ public class PostService {
         return postRepository.findPostById(id).orElse(Post.emptyObject());
     }
 
-//    public Post writePost(PostDto) {
-//
-//    }
+    public Post writePost(PostDto postDto) {
+
+        Post post = new Post();
+
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+
+        postRepository.save(post);
+
+        return post;
+    }
+
+    public Post updatePost(PostDto postDto) {
+
+        Post post = postRepository.findPostById(postDto.getId()).orElse(Post.emptyObject());
+
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+
+        postRepository.save(post);
+
+        return post;
+    }
+
 
 }
