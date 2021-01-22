496672fa868d1409ae1649fece161ca135459d90
FoeverNa <foevna@naver.com>
2021-01-22 12:17:56 +0900

    feat: Add getPosts

--- src/main/java/com/fastcampus/blog/service/PostService.java
+++ src/main/java/com/fastcampus/blog/service/PostService.java
@@ -1,14 +1,21 @@
 package com.fastcampus.blog.service;
 
+import com.fastcampus.blog.model.Post;
 import com.fastcampus.blog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+
+    public List<Post> getPosts(){
+        return postRepository.findAll();
+    }
 
 
 }
