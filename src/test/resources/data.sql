1c87477dc3469ccf58b380673d1661bed7730d29
FoeverNa <foevna@naver.com>
2021-01-22 18:01:49 +0900

    test : add deletePostTest

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,3 +4,4 @@ Insert Into Member(id, email, nickname, password) values (1L, 'martin@fastcampus
 Insert Into Comment(id, content, member_id) values (1L, '제목입니다', 1L)
 
 Insert Into Post(id, title, content) values (1L, 'title', 'content')
+Insert Into Post(id, title, content) values (3L, 'deleteTitle', 'deleteContent')
